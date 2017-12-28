7225d300e7f05036201194f202d1e9637159362f
Phoenix <fphoenix923@hotmail.com>
2017-12-28 11:01:50 +0800

    加入專案檔案。

---
+++ Clock/ClockViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Clock
+{
+	class ClockViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public int HourAngle { get; private set; }
+		public int MinuteAngle { get; private set; }
+		public int SecondAngle { get; private set; }
+
+		public void SetTime(DateTime time)
+		{
+			this.HourAngle = (time.Hour % 12) * 360 / 12;
+			this.MinuteAngle = time.Minute * 360 / 60;
+			this.SecondAngle = time.Second * 360 / 60;
+
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.HourAngle)));
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.MinuteAngle)));
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.SecondAngle)));
+		}
+	}
+}
---
+++ Clock/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reactive.Linq;
+
+namespace Clock
+{
+	/// <summary>
+	/// MainWindow.xaml 的互動邏輯
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private IDisposable m_Observable;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var viewModel = new ClockViewModel();
+
+			this.DataContext = viewModel;
+
+			this.m_Observable = Observable.Interval(TimeSpan.FromSeconds(1))
+				.Subscribe(v =>
+				{
+					Dispatcher.Invoke(() =>
+					{
+						viewModel.SetTime(DateTime.Now);
+					});
+
+				});
+		}
+	}
+}
